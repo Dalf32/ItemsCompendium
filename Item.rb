bf509fcbf9b26f1551ebc8f4cf1e54dd4aad94f8
Dalf32 <kylepmullins@gmail.com>
2013-05-31 15:50:23 -0400

    Pulled String and Array extension methods out of Item.rb. Also fixed capitalization of 'S

--- Item.rb
+++ Item.rb
@@ -1,44 +1,6 @@
 #Item.rb
 
-##
-# Adds titlecase and pretty functions to String for easier formatting during print statements.
-##
-class String
-  ##
-  # Quick and dirty titlecase function.
-  ##
-	def titlecase
-		gsub(/\w+/){|word| word.capitalize}
-	end
-
-  ##
-  # Replaces underscores with spaces and titlecases the string.
-  ##
-	def pretty
-		gsub('_', ' ').titlecase
-	end
-end
-
-##
-# Adds include_similar? function to Array for more robust searching.
-##
-class Array
-  ##
-  # Same as include? except returns true if the Array contains a value that
-  # either starts with or otherwise includes the key.
-  ##
-  def include_similar?(key)
-    retval = false
-
-    each{|value|
-      if value.start_with?(key) || value.include?(key)
-        retval = true
-      end
-    }
-
-    retval
-  end
-end
+require_relative 'Extensions'
 
 ##
 # This class holds all the data for a single Item in the compendium and allows checking the Item for a match with some
